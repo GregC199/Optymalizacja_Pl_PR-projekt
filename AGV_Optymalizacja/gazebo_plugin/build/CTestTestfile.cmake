93d4cbfac4f202ea0bfece1dcbf2985c87ad51cb
GregC199 <cieslargrzegorz199@gmail.com>
2022-01-24 09:31:40 +0100

    dowiazania

--- AGV_Optymalizacja/gazebo_plugin/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/greg/git/Smart-Factory/Smart_Factory_projekt/gazebo_plugin/src
-# Build directory: /home/greg/git/Smart-Factory/Smart_Factory_projekt/gazebo_plugin/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("agv_ppot")
--- AGV_Optymalizacja/gazebo_plugin/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/greg/git/Smart-Factory/Smart_Factory_projekt/gazebo_plugin/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/noetic'.split(';') if '/opt/ros/noetic' != '' else []
