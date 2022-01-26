85083de9b4b9f9e1c8ef8efe5849fc60abf4b871
GregC199 <cieslargrzegorz199@gmail.com>
2022-01-26 21:39:48 +0100

    Implementacja algorytmu planownia scieżki A*. Zmiana parametru przyciągania celu. Chwilowo nie działa sterowanie z zadawaniem celu z interfejsu. Po uruchomieniu jedzie do celu wpisanego w kodzie.

---
+++ AGV_Optymalizacja/gazebo_plugin/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/greg/git/Optymalizacja_Pl_PR-projekt/AGV_Optymalizacja/gazebo_plugin/src
+# Build directory: /home/greg/git/Optymalizacja_Pl_PR-projekt/AGV_Optymalizacja/gazebo_plugin/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("agv_ppot")
---
+++ AGV_Optymalizacja/gazebo_plugin/build/agv_ppot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/greg/git/Optymalizacja_Pl_PR-projekt/AGV_Optymalizacja/gazebo_plugin/src/agv_ppot
+# Build directory: /home/greg/git/Optymalizacja_Pl_PR-projekt/AGV_Optymalizacja/gazebo_plugin/build/agv_ppot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
